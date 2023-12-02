b7463a56135cf6a7391356ed635af51f744e4fc3
Florian Lefevre <contact@flefevre.fr>
2023-12-02 18:15:20 +0100

    feat: improve vscode install script

--- install_vscode_dev_container.sh
+++ install_vscode_dev_container.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# oh-my-zsh & plugins
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+
 cp ./.zshrc ~
 
+# Not cool but i do not know how to do it better
+cat ~/.zshrc_snippets >> ~/.zshrc
+rm ~/.zshrc_snippets
